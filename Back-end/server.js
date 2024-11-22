e23ce47add117781a576ecc45125cfb17dacc4a3
ammar5910 <ammar5910s@gmail.com>
2024-11-22 20:41:59 +0400

    Upd 8
    Middleware has been added
    Environment has been added

--- Back-end/server.js
+++ Back-end/server.js
@@ -6,13 +6,23 @@ const { connectDB } = require('./config/db');
 
 const lessonRoutes = require('./routes/lessonRoutes');
 const orderRoutes = require('./routes/orderRoutes');
+const { loggerMiddleware } = require('./middleware/logger');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(loggerMiddleware);
+app.use('/static', express.static('public')); // Serve static files
 
 // Connect to Database
 connectDB();
 
+// Routes
+app.use('/api/lessons', lessonRoutes);
+app.use('/api/orders', orderRoutes);
 
 // Start Server
 app.listen(PORT, () => {
