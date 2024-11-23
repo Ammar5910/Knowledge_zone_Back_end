d30e5a6134c50c6807ef28f8064e0da2421e136c
ammar5910 <ammar5910s@gmail.com>
2024-11-23 10:56:13 +0400

    Upd 13
    The server has been updated

--- Back-end/server.js
+++ Back-end/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const { connectDB } = require('./config/db');
+const path = require("path");
 
 const lessonRoutes = require('./routes/lessonRoutes');
 const orderRoutes = require('./routes/orderRoutes');
@@ -15,7 +16,9 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(loggerMiddleware);
-app.use('/static', express.static('public')); // Serve static files
+
+// Static Middleware for serving images
+app.use("/static/images", express.static(path.join(__dirname, "public", "images")));
 
 // Connect to Database
 connectDB();
@@ -23,6 +26,17 @@ connectDB();
 // Routes
 app.use('/api/lessons', lessonRoutes);
 app.use('/api/orders', orderRoutes);
+
+// Error Handling for Missing Static Files
+app.use("/static/images", (req, res) => {
+    res.status(404).json({ error: "Image not found" });
+  });
+  
+  // Default error handler
+  app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: "Something went wrong!" });
+  });
 
 // Start Server
 app.listen(PORT, () => {
